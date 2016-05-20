148fd971b51fe68855eb0f4737d8d2ce9669efb0
Apple <apple@cdy.local>
2016-05-20 10:25:24 +0800

    Initial Commit

---
+++ XiaLaLieBiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaLaLieBiao
+//
+//  Created by Apple on 16/5/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
